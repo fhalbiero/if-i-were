84ddbf8be7eec883ca7eaa529138e814f9f1f7da
fhalbiero <fhalbiero@gmail.com>
2019-09-24 15:01:01 -0300

    Updates

---
+++ precache-manifest.4ce7d794b056689db0997e954d079376.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a8533df4477e2d11b9532d0490bc1166",
+    "url": "/if-i-were/index.html"
+  },
+  {
+    "revision": "267d46ca2d6e887f868b",
+    "url": "/if-i-were/static/css/main.77675fda.chunk.css"
+  },
+  {
+    "revision": "72f538768305ce64879b",
+    "url": "/if-i-were/static/js/2.ea8140fa.chunk.js"
+  },
+  {
+    "revision": "267d46ca2d6e887f868b",
+    "url": "/if-i-were/static/js/main.0a5bf97f.chunk.js"
+  },
+  {
+    "revision": "2cf968bcf8af4aded8c8",
+    "url": "/if-i-were/static/js/runtime~main.5de3326e.js"
+  }
+]);
